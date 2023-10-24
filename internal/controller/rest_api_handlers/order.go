2cb58e53b9cb9198369ea995ab2a92d464b52ab4
Dzmitry Chystsik <chystsik@gmail.com>
2023-10-24 18:40:46 +0300

    check order number with Luhn algorithm

--- internal/controller/rest_api_handlers/order.go
+++ internal/controller/rest_api_handlers/order.go
@@ -2,6 +2,7 @@ package restapihandlers
 
 import (
 	"context"
+	"errors"
 	"io"
 	"net/http"
 	"strconv"
@@ -10,6 +11,10 @@ import (
 	"github.com/Chystik/gophermart/internal/models"
 	"github.com/Chystik/gophermart/internal/usecase"
 	"github.com/Chystik/gophermart/pkg/logger"
+)
+
+var (
+	errNotValidLuhn = errors.New("not valid order number")
 )
 
 type orderRoutes struct {
@@ -26,31 +31,36 @@ func newOrderRoutes(oi usecase.OrderInteractor, l logger.AppLogger) *orderRoutes
 
 func (or *orderRoutes) uploadOrders(w http.ResponseWriter, r *http.Request) {
 	var order models.Order
+	var numberRaw []byte
+	var number int
 	var ctx = context.Background()
 	var err error
 
-	numberRaw, err := io.ReadAll(r.Body)
+	numberRaw, err = io.ReadAll(r.Body)
 	if err != nil {
 		errorJSON(w, err, http.StatusInternalServerError, or.logger)
 		return
 	}
 
 	// check number
-	_, err = strconv.Atoi(string(numberRaw))
+	number, err = strconv.Atoi(string(numberRaw))
 	if err != nil {
 		errorJSON(w, err, http.StatusUnprocessableEntity, or.logger)
 		return
 	}
 
-	order.Number = string(numberRaw)
-
-	claims, ok := r.Context().Value(key).(*models.AuthClaims)
-	if !ok {
-		errorJSON(w, err, http.StatusUnauthorized, or.logger)
+	// validate
+	if !valid(number) {
+		errorJSON(w, errNotValidLuhn, http.StatusUnprocessableEntity, or.logger)
 		return
 	}
 
-	order.User = claims.Login
+	order.Number = string(numberRaw)
+	order.User, err = getUserLogin(r.Context()) //claims.Login
+	if err != nil {
+		errorJSON(w, err, http.StatusUnauthorized, or.logger)
+		return
+	}
 
 	err = or.orderInteractor.Create(ctx, order)
 	if err != nil {
@@ -83,4 +93,28 @@ func (or *orderRoutes) downloadOrders(w http.ResponseWriter, r *http.Request) {
 	}
 
 	writeJSON(w, http.StatusOK, "application/json", orders, or.logger)
+}
+
+// Valid checks the order number using the Luhn algorithm
+func valid(number int) bool {
+	var luhn int
+
+	checksum := func(n int) int {
+		for i := 0; n > 0; i++ {
+			cur := n % 10
+
+			if i%2 == 0 {
+				cur = cur * 2
+				if cur > 9 {
+					cur = cur%10 + cur/10
+				}
+			}
+
+			luhn += cur
+			n = n / 10
+		}
+		return luhn % 10
+	}
+
+	return (number%10+checksum(number/10))%10 == 0
 }
