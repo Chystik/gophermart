1a21d7e26e611ae6ee752926d27334353fc0e530
Dzmitry Chystsik <chystsik@gmail.com>
2023-10-25 18:06:31 +0300

    fix autotest: add scheme to the accrual address

--- internal/infrastructure/webapi/options.go
+++ internal/infrastructure/webapi/options.go
@@ -1,9 +1,11 @@
 package webapi
 
+import "fmt"
+
 type Options func(a *accrual)
 
 func Address(adr string) Options {
 	return func(a *accrual) {
-		a.url = adr
+		a.url = fmt.Sprintf("http://%s", adr)
 	}
 }
