70105fdafc0967a96418845c2cd8d0806cde99a2
Dzmitry Chystsik <chystsik@gmail.com>
2023-10-26 02:50:32 +0300

    del dotEnv load

--- cmd/gophermart/env.go
+++ cmd/gophermart/env.go
@@ -1,20 +1,17 @@
 package main
 
 import (
-	"os"
-
 	"github.com/Chystik/gophermart/config"
 
 	"github.com/caarlos0/env"
-	"github.com/joho/godotenv"
 )
 
 func parseEnv(cfg *config.App) error {
-	if osEnv := os.Getenv("ENVIRONMENT"); osEnv == "dev" {
+	/* if osEnv := os.Getenv("ENVIRONMENT"); osEnv == "dev" {
 		err := godotenv.Load(".env.dev")
 		if err != nil {
 			return err
 		}
-	}
+	} */
 	return env.Parse(cfg)
 }
